330e4d12d12aa97bc65a10c65465dfdbe3a698bb
Mahdi25250 <mahdiiltireh324@gmail.com>
2025-12-29 11:24:06 +0100

    exercise 1

---
+++ Exercises React/src/UserCard.jsx
@@ -0,0 +1,13 @@
+
+
+const UserCard = () =>{
+    return(
+        // jsx
+        <>
+        <h2>John Deo</h2>
+        <p>John.doe@gmail.com</p>
+        </>
+    )
+}
+
+export default UserCard;
